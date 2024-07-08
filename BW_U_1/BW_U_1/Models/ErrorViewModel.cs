93dd94b95f10297383fd9e29c0f45bf60f8bcccd
mirko-amoroso <mirkoamoroso599@gmail.com>
2024-07-08 09:44:07 +0200

    primo_commit

---
+++ BW_U_1/BW_U_1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BW_U_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
